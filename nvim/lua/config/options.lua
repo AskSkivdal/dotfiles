371b32ef7e6a0cddc06f29e1399f54926551004d
AskSkivdal <mail@askskivdal.com>
2026-01-08 16:30:04 +0100

    Use four spaces insted of tabs

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
